e3fbb0451a04a2d4d31d528bb781d4074e91f445
aguiarpedrof <d2023009216@unifei.edu.br>
2026-03-14 15:16:57 -0300

    Função inicial (detectção e classificação)

---
+++ main.py
@@ -0,0 +1,31 @@
+from ultralytics import YOLO
+import cv2 # para captura em tempo real,futuramente
+import math
+import cvzone # para colocar a caixa de texto com o fundo
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280) 
+cap.set(4, 720) 
+
+model = YOLO("../yOLO-Weights/yolov8n.pt")
+
+
+while True:
+    suucccess, imagem = cap.read()
+    results = model(imagem, stream=True)
+    for i in results:
+        boxes = i.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            cv2.rectangle(imagem, (x1,y1),(x2,y2),(255,0, 255),3)
+            cv2.putText(imagem, "Objeto", (x1,y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255,0,255), 2)
+            conf = math.ceil(box.conf[0] *100)/100
+            print(conf)
+            cls = int(box.cls[0])
+            cvzone.putTextRect(imagem, f"{cls} {conf}",(max(0, x1),max(20, y1)))
+    cv2.imshow("Video", imagem)
+    cv2.waitKey(1)
+
+
+
